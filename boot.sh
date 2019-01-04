92e153cb1db8eb96314fee21a684eb1862235aa0
Duarte OC <duarteocarmo@gmail.com>
2019-01-04 08:55:21 +0100

    more workers

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source venv/bin/activate
 flask db upgrade
-exec gunicorn -b :5000 --access-logfile - --error-logfile - dev:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - dev:app -w 4
